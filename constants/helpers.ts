fb90d0d64f1c48f43c2f7dac94abf0a1d0a111fe
stanley.pageot@gmail.com <stanleypageot@Stanleys-MacBook-Pro.local>
2024-12-18 12:52:56 -0800

    update: timeline

--- constants/helpers.ts
+++ constants/helpers.ts
@@ -31,3 +31,68 @@ export const profileDetails = {
   profileSummary:
     "Experienced Software Engineer with demonstrated success in developing robust, secure, and scalable applications. Proven ability to work in fast-paced environments, adapt to different work environments, and ensuring projects are delivered on time.",
 };
+
+export const jobDetails = [
+  {
+    jobYear: "July 2023 - Present",
+    company: "Super Systems Inc/Semper Valens Solutions",
+    jobTitle: "Software Engineer",
+    jobSummary: `I developed backend servers with FastAPI and NestJS, improving
+          performance and security through best practices like rate-limiting and
+          authentication verification. I integrated React on the frontend,
+          optimizing performance with hooks and reusable components, and reduced
+          development time. I tested AI models with FastAPI, implemented
+          TypeScript to improve reliability, and documented projects on GitHub
+          for better collaboration. I containerized projects with Docker,
+          applied security best practices, and used Azure functions for
+          scalability. I also optimized state management with Redux, tested with
+          Jest and pytest, and worked closely with UX designers and product
+          managers to deliver user-friendly interfaces.`,
+  },
+  {
+    jobYear: "Oct 2022 - Jul 2023",
+    company: "Freelancer",
+    jobTitle: "Full Stack Web Developer",
+    jobSummary: `I created
+          client-side applications with React to reduce development time and
+          improve performance and security. I implemented NestJS, FastAPI, and
+          GraphQL in various projects, adding security features to prevent
+          unauthorized access and limit endpoint pings. I wrote documentation
+          for smooth project handovers, containerized projects with Docker for
+          consistency across environments, and applied security best practices.
+          I also built mobile applications with React Native for cross-device
+          consistency and maintained clear communication with clients, walking
+          them through each project's development process`,
+  },
+  {
+    jobYear: "Nov 2021 - Oct 2022",
+    company: "Chia Network",
+    jobTitle: "React Developer",
+    jobSummary: `I streamlined
+          development by creating reusable React components and implemented
+          Redux for efficient state management, improving performance and
+          reducing re-renders. I used Jest for client-side testing, minimizing
+          errors, and integrated Formik to reduce user input mistakes.
+          Collaborating with UI designers and product managers, I translated
+          design concepts into functional interfaces and contributed to scalable
+          ExpressJS server-side applications. I also conducted code reviews,
+          provided feedback on best practices, and suggested UI improvements to
+          enhance user experience, all while actively participating in the agile
+          development process.`,
+  },
+  {
+    jobYear: "May 2020 - Nov 2021",
+    company: "Interviewer",
+    jobTitle: "React Developer",
+    jobSummary: `I implemented
+          React Native and React for mobile and web applications, optimizing
+          performance to reduce loading times and enhance overall efficiency. By
+          integrating Redux for centralized state management, I made debugging
+          easier and improved error handling. I decreased development time by
+          using Bootstrap and utilized TypeScript to reduce user errors and
+          bugs. Additionally, I optimized UI rendering and data fetching for
+          better performance and provided valuable UI recommendations to enhance
+          the user experience. Completing tasks on time helped accelerate
+          production.`,
+  },
+];
